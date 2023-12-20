1088f0898d47883c9ecb2683c3c535caf5f95895
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2023-12-20 21:55:00 +0100

    Added filelocation
    Now you can open up a file selector but cant select a file or folder

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.popup import Popup
 
 class MyApp(App):
     def build(self):
@@ -11,18 +13,22 @@ class MyApp(App):
 
         # Create text input fields
         self.text_input1 = TextInput(multiline=False, hint_text='Enter text 1')
-        self.text_input2 = TextInput(multiline=False, hint_text='Enter text 2')
-
-        # Create a button
-        button = Button(text='Run Code', on_press=self.run_code)
+        self.text_input2 = TextInput(multiline=False, hint_text='File location will be displayed here.', readonly=True)
 
         # Create a label for displaying the result
         self.result_label = Label(text='Result will be displayed here.')
 
+        # Create a button to run code
+        run_button = Button(text='Run Code', on_press=self.run_code)
+
+        # Create a button to open the file explorer
+        file_button = Button(text='Open File Explorer', on_press=self.open_file_explorer)
+
         # Add widgets to the layout
         layout.add_widget(self.text_input1)
         layout.add_widget(self.text_input2)
-        layout.add_widget(button)
+        layout.add_widget(file_button)
+        layout.add_widget(run_button)
         layout.add_widget(self.result_label)
 
         return layout
@@ -38,6 +44,25 @@ class MyApp(App):
 
         # Display the result in the label
         self.result_label.text = result
+
+    def open_file_explorer(self, instance):
+        # Create a popup for the file explorer
+        file_chooser = FileChooserListView()
+        file_popup = Popup(title='Select File', content=file_chooser, size_hint=(0.9, 0.9), auto_dismiss=False)
+
+        # Bind the selection event to set the file location
+        file_chooser.bind(on_submit=lambda x: self.set_file_location(file_chooser, file_popup))
+
+        # Open the file explorer popup
+        file_popup.open()
+
+    def set_file_location(self, file_chooser, file_popup):
+        # Set the selected file location to the text input
+        selected_file = file_chooser.selection and file_chooser.selection[0] or ''
+        self.text_input2.text = selected_file
+
+        # Close the file explorer popup
+        file_popup.dismiss()
 
 if __name__ == '__main__':
     MyApp().run()
